415ff282a551539b6e3f7a81a85c6788d2da25d4
Seanlinsanity <seanlin1016@gmail.com>
2019-10-20 17:48:20 +0800

    Initial Commit

---
+++ InstagramHomeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramHomeSwiftUI
+//
+//  Created by Sean on 2019/10/20.
+//  Copyright © 2019 Sean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
