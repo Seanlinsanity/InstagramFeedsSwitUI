5a5acb4e6f8e80c7e584b52e1c9bf10a4b09649c
Seanlinsanity <seanlin1016@gmail.com>
2019-10-20 18:24:40 +0800

    setup navigation bar

--- InstagramHomeSwiftUI/ContentView.swift
+++ InstagramHomeSwiftUI/ContentView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                Text("Hello World")
+                Text("Hello World")
+                Text("Hello World")
+            }.navigationBarTitle(Text("Instagram"), displayMode: .inline)
+                .navigationBarItems(leading: Button(action: {
+                    print(123)
+                }, label: {
+                    Image("camera")
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                }), trailing: Button(action: {
+                    print(456)
+                }, label: {
+                    Image("send")
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                }))
+        }
     }
 }
 
